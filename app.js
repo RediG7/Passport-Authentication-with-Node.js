aec49fc6e265c8c39b60868fba7bc78b62ba2499
RediG7 <guleksiredi7@gmail.com>
2021-01-04 13:18:20 +0100

    Configure and Connect DB

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+require('dotenv').config()
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
 // Routes
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/users', require('./routes/users'));"comm"
 
 const PORT = process.env.PORT || 3000;
 
