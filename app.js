01c540293eaa94e085b5e30e8f57a2ce1de06fa2
RediG7 <guleksiredi7@gmail.com>
2021-01-04 20:33:04 +0100

    Add global vars, config Passport, init express session

--- app.js
+++ app.js
@@ -2,25 +2,60 @@ require('dotenv').config()
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
-// DB Config
-const db = require('./config/keys').MongoURI;
+// Passport config
+require('./config/passport')(passport);
 
-// Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express Session
+app.use(session({
+  secret: 'keyboard cat', // this does not matter
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));"comm"
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
